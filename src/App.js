52283d9433d0da396b1d1297e01846efa44c4861
Casey <caseyclinga@gmail.com>
2020-12-08 14:45:39 -0800

    Completed app page with functional routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Bio from './components/PhageTherapy/Bio';
+import Resources from './components/PhageTherapy/Resources';
+import BoardOfDirectors from './components/About/BoardOfDirectors';
+import ScientificAdvisoryboard from './components/About/ScientificAdvisoryBoard';
+import Donate from './components/Donate';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello world!</h1>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/phagetherapy/bio' component={Bio} />
+          <Route exact path='/phagetherapy/resources' component={Resources} />
+          <Route exact path='/about/board' component={BoardOfDirectors} />
+          <Route exact path='/about/sab' component={ScientificAdvisoryboard} />
+          <Route exact path='/donate' component={Donate} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
