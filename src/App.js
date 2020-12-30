9b37d1f51c80e1c959cce143a98bd816107e309a
Casey <caseyclinga@gmail.com>
2020-12-29 18:02:39 -0800

    Added FAQ page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Home from './components/Home/Home';
 import Bio from './components/PhageTherapy/Bio/Bio';
-import Resources from './components/PhageTherapy/Resources/Resources';
+import FAQ from './components/PhageTherapy/FAQ/FAQ';
 import BoardOfDirectors from './components/About/BoardOfDirectors/BoardOfDirectors';
 import ScientificAdvisoryboard from './components/About/ScientificAdvisoryBoard/ScientificAdvisoryBoard';
 import Contact from './components/Contact/Contact';
@@ -21,7 +21,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/phagetherapy/bio' component={Bio} />
-          <Route exact path='/phagetherapy/resources' component={Resources} />
+          <Route exact path='/phagetherapy/faqs' component={FAQ} />
           <Route exact path='/about/board' component={BoardOfDirectors} />
           <Route exact path='/about/sab' component={ScientificAdvisoryboard} />
           <Route exact path='/contact' component={Contact} />
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -30,58 +30,55 @@ class NavbarPage extends Component {
   render() {
     return (
       <MDBNavbar className='navbar' color='stylish-color' dark expand='md'>
-        {/* <MDBContainer> */}
-        <MDBNavbarBrand className='navbrand ml-0 ml-md-5' href='/'>
-          {/* <img src={logo} className='logo' /> */}
-          PBRF
-        </MDBNavbarBrand>
-        <MDBNavbarToggler onClick={this.toggleCollapse} />
-        <MDBCollapse id='navbarCollapse3' isOpen={this.state.isOpen} navbar>
-          <MDBNavbarNav right>
-            <MDBNavItem>
-              <MDBDropdown>
-                <MDBDropdownToggle nav caret className='white-text '>
-                  <div className='d-md-inline '>Phage Therapy</div>
-                </MDBDropdownToggle>
-                <MDBDropdownMenu className='dropdown-default'>
-                  <MDBDropdownItem href='/phagetherapy/bio'>
-                    What is it?
-                  </MDBDropdownItem>
-                  <MDBDropdownItem href='/phagetherapy/resources'>
-                    Resources
-                  </MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBDropdown>
-                <MDBDropdownToggle nav caret className='white-text '>
-                  <div className='d-md-inline' title='About us'>
-                    About Us
-                  </div>
-                </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <MDBDropdownItem href='/about/board'>
-                    Board Of Directors
-                  </MDBDropdownItem>
-                  <MDBDropdownItem href='/about/sab'>
-                    Scientific Advisory Board
-                  </MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavItem>
+        <MDBContainer>
+          <MDBNavbarBrand className='navbrand ' href='/'>
+            {/* <img src={logo} className='logo' /> */}
+            PBRF
+          </MDBNavbarBrand>
+          <MDBNavbarToggler onClick={this.toggleCollapse} />
+          <MDBCollapse id='navbarCollapse3' isOpen={this.state.isOpen} navbar>
+            <MDBNavbarNav right>
+              <MDBNavItem>
+                <MDBDropdown>
+                  <MDBDropdownToggle nav caret className='white-text '>
+                    <div className='d-md-inline '>Phage Therapy</div>
+                  </MDBDropdownToggle>
+                  <MDBDropdownMenu className='dropdown-default'>
+                    <MDBDropdownItem href='/phagetherapy/bio'>
+                      What is it?
+                    </MDBDropdownItem>
+                    <MDBDropdownItem href='/phagetherapy/faqs'>
+                      FAQs
+                    </MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBDropdown>
+                  <MDBDropdownToggle nav caret className='white-text '>
+                    <div className='d-md-inline' title='About us'>
+                      About Us
+                    </div>
+                  </MDBDropdownToggle>
+                  <MDBDropdownMenu>
+                    <MDBDropdownItem href='/about/board'>
+                      Board Of Directors
+                    </MDBDropdownItem>
+                    <MDBDropdownItem href='/about/sab'>
+                      Scientific Advisory Board
+                    </MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+              </MDBNavItem>
 
-            <MDBNavItem>
-              <MDBNavLink
-                className='white-text mr-0 mr-md-5 mr-xl-8'
-                to='/contact'
-              >
-                Contact
-              </MDBNavLink>
-            </MDBNavItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-        {/* </MDBContainer> */}
+              <MDBNavItem>
+                <MDBNavLink className='white-text ' to='/contact'>
+                  Contact
+                </MDBNavLink>
+              </MDBNavItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
       </MDBNavbar>
     );
   }
